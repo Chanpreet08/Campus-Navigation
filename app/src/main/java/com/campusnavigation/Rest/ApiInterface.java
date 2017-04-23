b208809d5f226c2df81d5469a16067c824d9aa8f
Chanpreet08 <chanpreet.chhabra@gmail.com>
2017-04-23 16:42:32 +0530

    Rest api client

--- app/src/main/java/com/campusnavigation/Rest/ApiInterface.java
+++ app/src/main/java/com/campusnavigation/Rest/ApiInterface.java
@@ -1,10 +1,18 @@
 package com.campusnavigation.Rest;
 
+import com.campusnavigation.Model.MapRequest;
+import com.campusnavigation.Response.MapResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 /**
  * Created by cc on 23/4/17.
  */
 
 public interface ApiInterface {
 
-
+    @POST("/loaction")
+    Call<MapResponse> getLatLog(@Body MapRequest mapRequest);
 }
